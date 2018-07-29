114b195d4829568d3dd6e3a71a4ddd52eafc6712
valros76 <valerian.dufrene@gmail.com>
2018-07-29 18:46:14 +0200

    Ajout de views/homeViews

---
+++ views/homeView.php
@@ -0,0 +1,25 @@
+<?php
+    $title="Acceuil";
+    $locateCss="css/style.css";
+?>
+
+<?php ob_start();
+    echo '<h1>Acceuil</h1>';
+$header = ob_get_clean();?>
+
+<?php ob_start();
+    echo '<article>
+            <p>
+                Bienvenue sur mon blog.
+                <br/>
+                Je me nomme Jean Forteroche, je suis acteur et écrivain.
+                <br/>
+                J\'ai créé ce blog pour écrire mon prochain roman qui se nommera "Billet simple pour l\'Alaska".
+                <br/>
+                Ce blog fera office de "livre virtuel", j\'écrirais mon livre chapitre par chapitre et rendrais le contenu accessible ici.
+                <br/>
+                Je vous souhaite une bonne visite et une bonne lecture.
+            </p>
+        </article>';
+$content = ob_get_clean();?>
+<?php require('templates/home.php'); ?>
