c38e2e90966368a4061e1fdb22ee1772343ab6ba
valros76 <valerian.dufrene@gmail.com>
2018-07-29 21:18:43 +0200

    Ajout de la base d'un manager de commentaires

---
+++ models/managers/commentsManager.php
@@ -0,0 +1,32 @@
+<?php
+class CommentsManager{
+    private $_bdd;
+
+    public function __construct($bdd){
+        $this->setBdd($bdd);
+    }
+
+    public function add(Comment $comment){
+
+    }
+
+    public funtion delete(Comment $comment){
+
+    }
+
+    public function get($info){
+
+    }
+
+    public function getList(){
+
+    }
+
+    public function update(Comment $comment){
+
+    }
+
+    public function setBdd($bdd){
+        this->_bdd = $bdd;
+    }
+}?>
