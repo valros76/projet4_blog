3ae86ff7c8fcd5a9b370348ab6cb816daffce87d
valros76 <valerian.dufrene@gmail.com>
2018-07-29 18:38:58 +0200

    Ajout de templates/home.php

---
+++ templates/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="<?= $locateCss; ?>"/>
+    <title><?= $title; ?></title>
+</head>
+<body>
+    <div id="containerHome">
+        <header id="headHome">
+            <?= $header; ?>
+        </header>
+
+        <section id="contentHome">
+            <?= $content; ?>
+        </section>
+    </div>
+</body>
+</html>
