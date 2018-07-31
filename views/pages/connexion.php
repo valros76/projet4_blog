b3d4ceb532fe914d373172f116c08382d1bc165a
valros76 <valerian.dufrene@gmail.com>
2018-07-31 20:13:20 +0200

    Ajout d'une redirection vers la page inscription.php

--- views/pages/connexion.php
+++ views/pages/connexion.php
@@ -7,6 +7,31 @@
     require('../../models/bdd.php');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
+
+    if(isset($_POST['pseudo']) && isset($_POST['password']) && isset($_POST['connect'])){
+        $pseudo = $_POST['pseudo'];
+        $req = $bdd->prepare('SELECT id,mdp FROM users WHERE nom = :nom');
+        $req->execute(array(
+            'pseudo' => $pseudo));
+        $resultat = $req->fetch(PDO::FETCH_ASSOC);
+
+        $isPasswordCorrect = password_verify($_POST['password'], $resultat['password']);
+
+        if(!$resultat){
+            echo 'Mauvais identifiant / Mauvais mot de passe.';
+        }
+        else{
+            if($isPasswordCorrect){
+                session_start();
+                $_SESSION['id'] = $resultat['id'];
+                $_SESSION['pseudo'] = $pseudo;
+                echo 'Vous êtes connecté !';
+            }
+            else{
+                echo 'Mauvais identifiant ou mot de passe.';
+            }
+        }
+    }
    
 ?>
 
@@ -50,9 +75,9 @@ $postComment = ob_get_clean();?>
 <?php ob_start();
     echo '
         <fieldset>
-            <legend>Pages</legend>
+            <legend>Inscription</legend>
             <ul>
-                <li><a href="">Chapitre 1</a></li>
+                <li><a href="inscription.php">S\'inscrire</a></li>
             </ul>
         </fieldset>
     ';
