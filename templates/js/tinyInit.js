240a1c5ca81295f640933ef5485025d85e3f1e07
valros76 <valerian.dufrene@gmail.com>
2018-08-10 16:48:53 +0200

    Ajout d'un dossier js avec tiniInit.js

---
+++ templates/js/tinyInit.js
@@ -0,0 +1,10 @@
+tinymce.init({ 
+    selector:'.myTextarea',
+    theme: 'modern',
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor textcolor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table contextmenu paste code help wordcount'
+      ],
+      toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help'
+})
