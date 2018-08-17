24d868b77872e90c0400b6f07b85bbdd7f95aea4
valros76 <valerian.dufrene@gmail.com>
2018-08-17 12:23:12 +0200

    Ajout de paramètres à l'init de tinyMCE

--- templates/home.php
+++ templates/home.php
@@ -9,6 +9,9 @@
     <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=aodwpl4fidsfjdc4lsy7dwdfx22id12dxjh7t7s5vietisvy"></script>
     <script>tinymce.init({ 
         selector:'textarea',
+        forced_root_block : '',
+        force_br_newlines : true,
+        force_p_newlines : false,
         themes:'advanced',
         width:'99.6%',
         height:'auto'
