be3d52074e88c1bfac25b956c323666267985e70
valros76 <valerian.dufrene@gmail.com>
2018-08-30 17:03:56 +0200

    Réaménagement des logs bdd pour le reformatage du projet et avant sécurisation de la connexion BDD

--- models/bdd.php
+++ models/bdd.php
@@ -1,6 +1,6 @@
 <?php
         try{
-            $bdd = new PDO('mysql:host=localhost;dbname=id6846891_blog_ecrivain;charset=utf8', 'id6846891_valros76', 'granpeper4', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $bdd = new PDO('mysql:host=localhost;dbname=blog_ecrivain;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
         }
         catch(Exception $e){
             die('Erreur : ' . $e->getMessage());
